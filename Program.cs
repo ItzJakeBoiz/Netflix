85271297e4d8ddf0f7fd1f7706e77c848daa4a0f
Beansados <zfrankel2016@gmail.com>
2022-03-26 17:13:38 +0000

    Updated UI

--- Program.cs
+++ Program.cs
@@ -17,23 +17,6 @@ namespace Netflix
         [STAThread]
         static void Main()
         {
-            string path = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
-            if (!File.Exists(path+ "/AxInterop.WMPLib.dll"))
-            {
-                WebClient myWebClient = new WebClient();
-                myWebClient.DownloadFile("https://backend-server.18jchadwick.repl.co/download/1", path + "/AxInterop.WMPLib.dll");
-
-            }
-            if (!File.Exists(path + "/Interop.WMPLib.dll"))
-            {
-                WebClient myWebClient = new WebClient();
-                myWebClient.DownloadFile("https://backend-server.18jchadwick.repl.co/download/2", path + "/Interop.WMPLib.dll");
-            }
-
-
-
-
-
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
